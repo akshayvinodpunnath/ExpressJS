69a6407c2017fbda21582acb580de068cf50afe0
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2019-09-25 06:07:09 -0700

    5th commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5d8a0922c8c8b00e66ab39c0')
      .then(user => {
-       req.user = user;
+       req.user = new User(user.name, user.email, user.cart, user._id);
        //console.log(user)
        next();
      })
